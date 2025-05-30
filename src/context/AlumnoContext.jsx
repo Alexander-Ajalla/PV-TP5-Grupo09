a504441146f2875b8a7acec36fcc31e471da778b
Ivo <ivothaielvicencio@gmail.com>
2025-05-30 19:33:30 -0300

    refactor: agregado un array con datos iniciales al contexto de Alumno [Ivo Thaiel Vicencio Rosas]

--- src/context/AlumnoContext.jsx
+++ src/context/AlumnoContext.jsx
@@ -4,7 +4,49 @@ export const AlumnoContext = createContext();
 
 export const AlumnoProvider = ({ children }) => {
   const [mensaje, setMensaje] = useState("");
-  const [alumnos, setAlumnos] = useState([]);
+   const alumnosIniciales = [
+    {
+      id: 1,
+      lu: "APU004576",
+      nombre: "Ariel Maximiliano",
+      apellido: "Sánchez",
+      curso: "Programación Visual",
+      email: "maxisanchezms.ms@gmail.com",
+      domicilio: "Soldado Sevilla 29",
+      telefono: "3884625359"
+    },
+    {
+      id: 2,
+      lu: "APU004890",
+      nombre: "Carlos Matias",
+      apellido: "Alvarez",
+      curso: "Programacion Visual",
+      email: "43269789@fi.unju.edu.ar",
+      domicilio: "B° 20 Viviendas S/N",
+      telefono: "3884379293"
+    },
+    {
+      id: 3,
+      lu: "5634",
+      nombre: "Lautaro",
+      apellido: "Barrientos",
+      curso: "Programación",
+      email: "barrientoslautaro8@gmail.com",
+      domicilio: "Alto Gorriti",
+      telefono: "3644369786"
+    },
+    {
+      id: 4,
+      lu: "APU005553",
+      nombre: "Ivo",
+      apellido: "Vicencio",
+      curso: "Programación Visual",
+      email: "ivothaielvicencio@gmail.com",
+      domicilio: "El picaflor 477",
+      telefono: "2995757328"
+    }
+  ];
+  const [alumnos, setAlumnos] = useState(alumnosIniciales);
 
   const mostrarMensaje = (texto) => {
     setMensaje(texto);
